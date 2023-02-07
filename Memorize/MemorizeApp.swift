a810f7a88355b979b4e75bada4a5ba3558ea7707
muhmp <defcoolz@gmail.com>
2023-02-07 20:21:26 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Muhammad Mappanyompa on 2023/02/07.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
