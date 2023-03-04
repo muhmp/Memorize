5fcf459d24be6f45354175c904a20191d43457bb
muhmp <defcoolz@gmail.com>
2023-03-04 20:14:09 +0900

    Combine Horizon and Vertical & add Action text

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -14,7 +14,10 @@ struct CardView : View {
     //define the faceup
     @State var isFaceUp: Bool = true
     
-    //Zstack
+    //reconstruct "emoji" by defining string
+    var content : String
+    
+    //Zstack for the content
     var body: some View {
         ZStack {
             //variable shape
@@ -25,8 +28,8 @@ struct CardView : View {
                 RoundedRectangle(cornerRadius: 20)
                     shape.fill().foregroundColor(.white)
                     shape.stroke(lineWidth:3)
-                    Text("✈️").font(.largeTitle)
-                
+                    //Text("✈️").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill()
             }
@@ -38,36 +41,63 @@ struct CardView : View {
 }
 
 
-//HStack
+//Content View
 struct ContentView: View {
+    //reconstruct "emoji" by defining array
+    var emojis = ["🚄","🚆","🛫","🚀","🚁","🛸","🛥️","🛵","🚲","🚚","🚗","🚙","🏎️","🚤","✈️","🚃"]
+    
+    @State var emojiCount = 4
+    
+    //HStack for the horizon cards
     var body: some View {
-        HStack{
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        //combine Vstack and Hstack
+        VStack{
+            HStack{
+                ForEach(emojis[..<emojiCount],id: \.self){
+                    emojis in CardView(content: emojis)
+                }
+            }
+            
+            //Button increase decrease with param action and label
+            
+            //HStack remove
+            HStack{
+                // Button for remove an emoji
+                Button(action:{
+                    emojiCount -= 1
+                },label:{
+                    VStack{
+                        Text("Remove")
+                        Text("Card")
+                    }
+                })
+                
+                // Button for adding an emoji
+                Button(action:{
+                    emojiCount += 1
+                },label:{
+                    VStack{
+                        Text("Add")
+                        Text("Card")
+                    }
+                })
+            }
+            
+            .padding(.horizontal)
+            .foregroundColor(.red)
         }
-        .padding(.horizontal)
-        .foregroundColor(.red)
     }
-}
+    
+    
 
-
-
-
-
-
-
-
-
-
-/** Contentview previews */
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
-        ContentView()
-            .preferredColorScheme(.light)
+    /** Contentview previews */
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .preferredColorScheme(.dark)
+            ContentView()
+                .preferredColorScheme(.light)
+        }
     }
+    
 }
-
